51b66bb6e051f2fd670dbb1fbc2e5656caca81e3
hoodielive <hoodielive@gmail.com>
2020-06-23 19:39:50 -0400

    Just what the doctor ordered. Now you can pick the day of a week by number.

--- Collections/array.cs
+++ Collections/array.cs
@@ -21,10 +21,12 @@ class Program
 
 		// List all the days of the week in order.
 
-		foreach (string day in daysOfWeek)
-		{
-			Console.WriteLine(day);
-		}
+		Console.WriteLine("Which day do you want to display?");
+		Console.Write("(Monday = 1, etc.) > ");
+		int iDay = int.Parse(Console.ReadLine());
+
+		string chosenDay = daysOfWeek[iDay];
+		Console.WriteLine($"That day is {chosenDay}");
 	}
 }
 
