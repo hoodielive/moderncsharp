815401799bcc8351764840dd07b910699c3edafa
hoodielive <hoodielive@gmail.com>
2020-06-22 23:22:48 -0400

    Teaching a class tonight. Used this as a teaching modality.

---
+++ Csharp/prax.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Tutorial 
+{
+	static void Main()
+	{
+		Console.WriteLine(FeetToInches("30"));
+	}
+
+	static string FeetToInches(string feet)
+	{
+		string result = feet; 
+		return result;
+	}
+}
