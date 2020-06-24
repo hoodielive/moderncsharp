dbe7472c9dcc7605d9395eaddc16ab47d701d6af
hoodielive <hoodielive@gmail.com>
2020-06-23 21:22:35 -0400

    So apparently, csvreader.split takes an array of chars.

--- Collections/TopTen/CsvReader.cs
+++ Collections/TopTen/CsvReader.cs
@@ -17,7 +17,7 @@ class CsvReader
 
 	public Country ReadCountryFromCsvLine(string csvLine)
 	{
-		string[] parts = csvLine.Split(',');
+		string[] parts = csvLine.Split(new char[] {','});
 
 		string name = parts[0];
 		string code = parts[1];
