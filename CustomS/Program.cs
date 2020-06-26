c4d7d7c5495cbc59ddd17d1e499b6eb9afb25048
hoodielive <hoodielive@gmail.com>
2020-06-25 23:08:37 -0400

    The Saga continues..

---
+++ CustomS/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CustomS
+{
+	public class UnitConverter
+	{
+		// Data member.
+
+		int ratio;
+
+		// Constructor.
+
+		public UnitConverter(int unitRatio)
+		{
+			ratio = unitRatio;
+		}
+
+		// Method.
+
+		public int Convert(int unit)
+		{
+			return unit * ratio;
+		}
+	}
+
+	class Test
+	{
+		static void Main()
+		{
+			UnitConverter feetToInchesConverter = new UnitConverter(12);
+			UnitConverter milesToFeetConverter = new UnitConverter(5280);
+
+			Console.WriteLine(feetToInchesConverter.Convert(30));
+			Console.WriteLine(feetToInchesConverter.Convert(100));
+			Console.WriteLine(feetToInchesConverter.Convert(milesToFeetConverter(1)));
+		}
+	}
+}
