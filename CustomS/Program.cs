3c198c5830d3fd0bd2c1c55a69851d2d82d6696d
hoodielive <hoodielive@gmail.com>
2020-06-25 23:44:27 -0400

    The difference between static and instance fields.

--- CustomS/Program.cs
+++ CustomS/Program.cs
@@ -32,7 +32,28 @@ namespace CustomS
 
 			Console.WriteLine(feetToInchesConverter.Convert(30));
 			Console.WriteLine(feetToInchesConverter.Convert(100));
-			Console.WriteLine(feetToInchesConverter.Convert(milesToFeetConverter(1)));
+			Console.WriteLine(feetToInchesConverter.Convert(milesToFeetConverter.Convert(1)));
+
+			Panda p1 = new Panda("Pan Dee");
+			Panda p2 = new Panda("Pan Dah");
+
+			Console.WriteLine(p1.Name);
+			Console.WriteLine(p2.Name);
+
+			Console.WriteLine(Panda.Population);
+		}
+	}
+
+	class Panda
+	{
+		public string Name; // Instance field. 
+		public static int Population; // Static Field.
+
+
+		public Panda(string n) // Constructor.
+		{
+			Name = n;  // Assign the Instance field.
+			Population += 1; // Increment the static population field.
 		}
 	}
 }
