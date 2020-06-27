06a9b3c68ca74bc6c2ce08deb1474980c1b32c41
hoodielive <hoodielive@gmail.com>
2020-06-27 14:31:30 -0400

    ahhhh

---
+++ DoesItExist/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DoesItExist
+{
+	class ProcessIt
+	{
+		public void Main(string[] args)
+		{
+			TimePeriod newTime = new TimePeriod(4, 5);
+			Console.WriteLine(newTime.ReturnTime1());
+		}
+	}
+
+	class TimePeriod
+	{
+		public TimePeriod(int time1, int time2)
+		{
+			int timeA = time1;
+			int timeB = time2;
+		}
+
+		public int ReturnTime1
+		{
+			get { return timeA; }
+			set { timeA = value; }
+		}
+	}
+}
