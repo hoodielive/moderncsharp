87831aaf73fb4983b2cea4fc98ce73b520354092
hoodielive <hoodielive@gmail.com>
2020-06-27 03:23:13 -0400

    I have ..

--- Lists/list_contains.cs
+++ Lists/list_contains.cs
@@ -9,7 +9,6 @@ namespace Lists
 		public static void Main(String[] args)
 		{
 			List<int> firstList = new List<int>();
-
 			firstList.Add(1);
 			firstList.Add(2);
 			firstList.Add(3);
@@ -18,6 +17,39 @@ namespace Lists
 			firstList.Add(6);
 
 			Console.Write(firstList.Contains(4));
+
+			Point p1 = new Point();
+			p1.X = 7;
+
+			Point p2 = p1;
+
+			Console.WriteLine(p1.X);
+			Console.WriteLine(p2.X);
+
+			p1.X = 9;
+
+			Console.WriteLine(p1.X);
+			Console.WriteLine(p2.X);
+
+			PointClass p3 = new PointClass();
+			p3.XPoint = 55;
+
+			PointClass p4 = p3;
+
+			Console.WriteLine(p3.XPoint);
+			Console.WriteLine(p4.YPoint);
+		}
+
+		public struct Point
+		{
+			public int X;
+			public int Y;
+		}
+
+		public class PointClass
+		{
+			public int XPoint;
+			public int YPoint;
 		}
 	}
 }
