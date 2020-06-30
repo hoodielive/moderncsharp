c23b12c696e8a16847dd572b4aabbc5eeffc421d
hoodielive <hoodielive@gmail.com>
2020-06-29 23:24:15 -0400

    Inheritance principles.

---
+++ Prax/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Prax
+{
+	class Objectify
+	{
+		public static void Main(string[] args)
+		{
+			Square s = new Square();
+			Rectangle r = s; // Up
+			Square s2 = (Square)r; // Down
+		}
+	}
+	class Rectangle
+	{
+		public int x = 10, y = 10;
+		public int GetArea() 
+		{
+			return x * y;
+		}
+	}
+
+	class Square : Rectangle {}
+}
---
+++ Prax/Player.cs
@@ -0,0 +1,9 @@
+
+public class Player 
+{
+	public string ReturnSomething(string name)
+	{
+		string result = name;
+		return result;
+	}
+}
