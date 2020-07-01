959ab1c762cf8525cad99d575a8bce1f219035f7
hoodielive <hoodielive@gmail.com>
2020-06-30 22:12:31 -0400

    Parallel programming with Vincent - with inheritance.

---
+++ VTank_Expanded/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VTank
+{
+    public class Test
+    {
+        public static void Main()
+        {
+            Test2 test2 = new Test2();
+            Console.WriteLine(test2.StartInt(12));
+            Console.WriteLine(test2.StartFloat(3.3f));
+        }
+    }
+}
