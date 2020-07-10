02402d49e5a27392986e97137b7e020b1d42e174
hoodielive <hoodielive@gmail.com>
2020-07-09 23:21:44 -0400

    Setup for polymorphism.

--- OdInterfacing/Developer.cs
+++ OdInterfacing/Developer.cs
@@ -11,11 +11,19 @@ namespace OdInterfacing
             this._mainDevLanguage = mainDevLanguage;
         }
 
-        public virtual void Code()
+        public virtual void Code(string firstExtraDevLanguage=null, string secondExtraDevLanguage=null)
         {
-            Console.WriteLine(string.Format("I am coding using my main dev language {0}.", this._mainDevLanguage));
+            if (firstExtraDevLanguage == null && secondExtraDevLanguage == null)
+            {
+                Console.WriteLine(string.Format("I am coding using my main dev language {0}.", this._mainDevLanguage));
+            }
+            else if (firstExtraDevLanguage != null && secondExtraDevLanguage != null)
+            {
+                Console.WriteLine(string.Format("I am coding using my main dev {0}. Additionally I am using {1} and {2} as an extra language", this._mainDevLanguage, this.firstExtraDevLanguage, this.secondExtraDevLanguage));
+                    
+                }))
+            }
         }
-    
     }
 
 }
