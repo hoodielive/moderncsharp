8a2bb44a1293a35356d6975d74543fffad7dedc8
hoodielive <hoodielive@gmail.com>
2020-07-15 22:20:31 -0400

    Okay, now it works

---
+++ Misc/Voodoo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Voodoo
+{
+
+		enum Status 
+		{
+			Alive,
+			Enabled, 
+			Disabled, 
+			Dead
+		}
+    class Program
+    {
+			static void Main(string[] args)
+			{
+
+				Status player = Status.Disabled; 
+
+				switch(player)
+				{
+					case Status.Alive:
+						Console.WriteLine("This thing is Alive.");
+						break;
+					case Status.Enabled:
+						Console.WriteLine("This thing is Enabled.");
+						break;
+					case Status.Disabled:
+						Console.WriteLine("This thing is Disabled.");
+						break;
+					case Status.Dead:
+						Console.WriteLine("This thing is Dead.");
+						break;
+					default:
+						Console.WriteLine("Get out of here!");
+					break;
+				}
+			}
+    }
+}
