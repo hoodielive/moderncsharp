530a17965a2ddfdd0eb19108c20dd1adc02bdc4f
hoodielive <hoodielive@gmail.com>
2020-07-16 23:03:28 -0400

    Structs can contain fields, functions, constructors, indexers, events, nested types and an implemented interface.

---
+++ RunTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System; 
+
+namespace RunTypes 
+{
+	class Withering
+	{
+		
+		public static void Main(string[] args)
+		{
+			Vector vector = new Vector();
+			vector.x = 10;
+			vector.y = 12;
+
+			Console.WriteLine("Vector X is in the size of {0}", vector.x);
+			Console.WriteLine("Vector Y is in the size of {0}", vector.y);
+			Console.WriteLine("Does Vector Z contain a value of 0 ?  {0}", vector.z);
+		}
+	}
+
+	struct Vector 
+	{
+		public int x; 
+		public int y;
+		public int z;
+	}
+}
