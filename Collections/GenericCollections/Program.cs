3c3380352a80f6f5a76e824c27ada8f9e842c093
hoodielive <hoodielive@gmail.com>
2020-07-19 03:19:59 -0400

    Library..

--- Collections/GenericCollections/Program.cs
+++ Collections/GenericCollections/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic; 
 
 namespace GenericCollections
 {
@@ -6,7 +8,24 @@ namespace GenericCollections
     {
         static void Main(string[] args)
         {
+					string[] comedians = {
+						"Leo Gallagher",
+						"Rodney Dangerfield",
+						"George Carlin",
+						"Lenny Bruce",
+						"Eddie Murphy",
+						"Louie Anderson"
+					};
+	
+					// TODO: Create the List.
+					List<string> strList = new List<string>(10);
 
+					// TODO: Add some elements to the list from an existing array.
+					strList.Add("Larry Fey");
+					strList.Add("Larry Fey");
+
+					// TODO: Examine the Count and Capacity.
+					Console.WriteLine("Count and Capacity: {0}, {1}", strList.Count, strList.Capacity);
         }
     }
 }
