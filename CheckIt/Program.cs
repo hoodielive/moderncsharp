5f7a5bec990a92425dee32fc6c4bfe5f2de6db73
hoodielive <hoodielive@gmail.com>
2020-07-19 03:30:53 -0400

    Made it..

---
+++ CheckIt/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+
+namespace CheckIt
+{
+	public class Bishop 
+	{
+		public static void Main(string[] args)
+		{
+		}
+		public string GetName(int ID)	
+		{
+			if (ID < name.Length)
+				return names[ID];
+			else
+				return String.Empty;
+		}
+		private string[] names = { "Osa Meji", "Oyeku Meji", "Ofun Meji" };
+	}
+}
