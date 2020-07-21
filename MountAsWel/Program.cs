b4b48a26207cd44a1bca5cbd9bd19b3e28dc2e60
hoodielive <hoodielive@gmail.com>
2020-07-21 16:14:18 -0400

    Lets work..

---
+++ MountAsWel/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MountAsWel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        	Console.WriteLine("Enter your name: "); 
+					var name = Console.ReadLine();
+					Console.WriteLine("Welcome {0}", name);
+        }
+    }
+}
