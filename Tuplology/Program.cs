e5bbc1853613c6d5ea30824602a3ecf91b530625
hoodielive <hoodielive@gmail.com>
2020-07-23 23:06:19 -0400

    I am disappointed

---
+++ Tuplology/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tuplology
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+					Tuple<(double, int)> t1 = (4.5, 3); 
+						Console.WriteLine($"Tuple with elements { t1.Item1 } and { t1.Item2 }.");
+						
+						Tuple<(double Sum, int Count)> t2 = (4.5, 3);
+						Console.WriteLine($"Sum of { t2.Count } and { t1.Sum }.");
+        }
+    }
+}
