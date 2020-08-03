dff086e1911acafcbfb14b4c2df6133022e4c15a
hoodielive <hoodielive@gmail.com>
2020-08-02 23:50:50 -0400

    Garbage code

---
+++ ThreadIt/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ThreadIt
+{
+    class Program
+    {
+				public static void ThreadMethod()
+				{
+					for (int i = 0; i < 10; i++)
+					{
+						Console.WriteLine($"ThreadProc: {i}");
+						Thread.Sleep(0);
+					}
+				}
+        static void Main(string[] args)
+        {
+        	Thread t = new Thread(new ThreadStart(ThreadMethod));
+					t.Start();
+
+					for (int i = 0; i < 4; i++)
+					{
+						Console.WriteLine("Main Thread: Do some work.");
+						Thread.Sleep(0);
+					}
+					t.Join();
+        }
+    }
+}
