6fd5024a3d767d85fa42362da805cee2daf35ac4
hoodielive <hoodielive@gmail.com>
2020-08-08 22:22:22 -0400

    Enums and Arrays.

---
+++ ImplementationOfPoly/MessAroundWithArrays.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ClassOnArrays
+{
+    int[] numbers = new int[5] { 4, 5, 6, 7, 8 };
+    int[] numbers2 = { 4, 5, 6, 7, 8 };
+
+    // Jagged Arrays.
+
+    int[][] jaggedArray = new int[3][];
+    jaggedArray[0] = new int[] { 1, 2, 4, 6, 8, 9 };
+    jaggedArray[1] = new int[] { 1, 2, 3, 4, 5 };
+    jaggedArray[2] = new int[] { 2, 4, 6, 12, 423, 23, 4123, 23 };
+
+
+    enum Days { Mon, Tue, Wed, Thurs, Fri, Sat, Sun };
+
+    
+}
