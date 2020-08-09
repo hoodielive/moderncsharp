905007b8c6dfdecb5b7def4da6da895170c6e147
hoodielive <hoodielive@gmail.com>
2020-08-08 22:50:59 -0400

    What in the call of duty is going on here?

--- ImplementationOfPoly/MessAroundWithArrays.cs
+++ ImplementationOfPoly/MessAroundWithArrays.cs
@@ -4,29 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class ClassOnArrays
+namespace ImplementationOfPoly
 {
-    int[] numbers = new int[5] { 4, 5, 6, 7, 8 };
-    int[] numbers2 = { 4, 5, 6, 7, 8 };
-
-    // Jagged Arrays.
-
-    int[][] jaggedArray = new int[3][];
-    // jaggedArray[0] = new int[] { 1, 2, 4, 6, 8, 9 };
-    // jaggedArray[1] = new int[] { 1, 2, 3, 4, 5 };
-    // jaggedArray[2] = new int[] { 2, 4, 6, 12, 423, 23, 4123, 23 };
-
-
-    enum Days { Mon, Tue, Wed, Thurs, Fri, Sat, Sun };
-
-    Days today = Days.Mon;
-
-    if (today == Days.Mon)
+    public class ClassOnArrays
     {
-        Console.WriteLine("Hey its Monday~!");
-    }
+        public void Aris()
+        {
+            int[] numbers = new int[5] { 4, 5, 6, 7, 8 };
+            int[] numbers2 = { 4, 5, 6, 7, 8 };
 
-    foreach (string s in Enum.GetNames(typeof(Days))) 
-    {
-        Console.WriteLine(s);
-    }
+            // Jagged Arrays.
+
+            int[][] jaggedArray = new int[3][];
+            jaggedArray[0] = new int[] { 1, 2, 4, 6, 8, 9 };
+            jaggedArray[1] = new int[] { 1, 2, 3, 4, 5 };
+            jaggedArray[2] = new int[] { 2, 4, 6, 12, 423, 23, 4123, 23 };
+
+        }
+
+        enum Days { Mon, Tue, Wed, Thurs, Fri, Sat, Sun };
+
+        Days today = Days.Mon;
+
+        if (today == Days.Mon)
+        {
+            Console.WriteLine("Hey its Monday~!");
+        }
+
+        foreach (string s in Enum.GetNames(typeof(Days))) 
+        {
+            Console.WriteLine(s);
+        }
+}
