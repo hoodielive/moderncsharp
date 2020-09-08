769ef87ef7a6ef93549bb28ee3b3c4124e54a8c0
hoodielive <hoodielive@gmail.com>
2020-09-07 22:51:53 -0400

    Studying the switch expression

---
+++ NewFeatures/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NewFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+		public void WorkWithSwitchExpression()
+		{
+			var species = new switch 
+			{
+
+			}
+		}
+    }
+}
