ca2a4ff502c72b540c5f14d197d45209f69e3800
hoodielive <hoodielive@gmail.com>
2020-10-25 11:24:31 -0400

    Warm up with a foreach loop.

--- Unity/Program.cs
+++ Unity/Program.cs
@@ -6,7 +6,10 @@ namespace Unity
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] fibarray = new int[] { 0, 1, 1, 2, 3, 5, 8, 13 };
+
+						foreach (int element in fibarray)
+							Console.WriteLine(element);
         }
     }
 }
