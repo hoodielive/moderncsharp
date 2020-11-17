93d1d5007152c7f9ffc2a95b09ece824f56f3b8c
hoodielive <hoodielive@gmail.com>
2020-11-16 22:42:26 -0500

    This do in remembrance of me.

---
+++ Magg/Magg.cs
@@ -0,0 +1,21 @@
+using System; 
+
+public class Maggot
+{
+	private string _administrator { get;  }
+	private string _attorney { get; set; }
+	public string new_attorney => false;
+
+	public bool isInherited()
+	{
+		bool strife = false;
+	}
+
+	public string RealLaw(string Justice, string Justi, string Law )
+	{
+			this.Justice = Justice; 
+			this.Justi = Justi; 
+			this.Law = Law;
+			isInherited();
+	}
+}
---
+++ Magg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Magg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
