502b8ed60270563cb9224287b9cd267db068f9bd
hoodielive <hoodielive@gmail.com>
2020-12-26 21:19:50 -0500

    Mosh was irritating today.

--- Undo/Program.cs
+++ Undo/Program.cs
@@ -6,6 +6,9 @@ namespace Undo
     {
         static void Main(string[] args)
         {
-
+            var number = "1234";
+            int i = Convert.ToInt32(number);
+            Console.WriteLine(i);
         }
+    }
 }
