b494239ec0e86a8f793dcd70388c148a9f0c5a93
hoodielive <hoodielive@gmail.com>
2021-01-02 17:12:31 -0500

    Um..

---
+++ Dragons/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+
+namespace Dragons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            private string _fortune;
+
+            public string GetString { get { return this._fortune; } set { this._fortune = value; } }
+
+            GetString = "Kalunga";
+        }
+    }
+}
--- Unity/SquareIt.cs
+++ Unity/SquareIt.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections;
 
 class Mephis
 {
+	float _fortune = 10.f;
+	System.Console.WriteLine(_fortune);
 }
