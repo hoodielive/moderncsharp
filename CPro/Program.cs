88898d8ca241e1b492cbcbff5bbaf18ba05d525c
hoodielive <hoodielive@gmail.com>
2021-01-10 16:11:11 -0500

    argghhhh..

--- CPro/Program.cs
+++ CPro/Program.cs
@@ -6,7 +6,57 @@ namespace CPro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool complete = true;
+            bool started = false;
+
+            if (complete)
+            {
+                int x = 5;
+                Console.WriteLine(x);
+            }
+            else if (started)
+            {
+                int x = 3;
+                Console.WriteLine(x);
+            }
+
+            StringBuilder sb = new StringBuilder("This is a new string");
+            sb.Replace(" ", _);
+            Console.WriteLine(sb);
+            PrintHi();
+            SNullColecse();
+            CalculateSizeOf();
+            GetObjectType();
+            HowDoesThatWork();
+        }
+
+        static void PrintHi()
+        {
+           Console.WriteLine("hi");
+        }
+
+        static void SNullColecse()
+        {
+            object obj1 = null;
+            object obj2 = obj1 ?? new object();
+            Console.WriteLine(obj2);
+        }
+
+        static void CalculateSizeOf()
+        {
+            Console.WriteLine(sizeof(int));
+        }
+
+        static void GetObjectType()
+        {
+            Console.WriteLine(typeof(int));
+        }
+
+        static void HowDoesThatWork()
+        {
+            // Operator Overloading.
+            Type sType = typeof(String);
+            System.Console.WriteLine(sType.FullName);
         }
     }
 }
--- CPro/ValidMainMethods.md
+++ CPro/ValidMainMethods.md
@@ -9,3 +9,35 @@ public static async Task<int> Main() {}
 public static async Task Main(string[] args) {}
 public static async Task<int> Main(string[] args) {}
 ```
+
+## Basic Builtin Types:
+- bool = System.Boolean
+- float = System.Single
+- int = System.Int32
+- string = System.String
+- char = System.Char
+
+```csharp
+Type sType = typeof(String);
+Console.WriteLine(sType.FullName);
+```
+
+## Math Functions
+Abs - absolute value - Math.Abs() or Mathf.Abs()
+Round - rounding - Math.Round()
+Floor - round down irrespective of the decimal value - Math.Floor()
+Ceil - round up to nearest int irrespective of the decimal value - Math.Ceil()
+Truncate - round to int irrespective of the decimal value - Math.Truncate(5.9) # 5
+Min - pick the lowest value - Math.Min(1, 3);
+Max - pick the highest value - Math.Max(1, 3);
+
+## String
+Under the hood a string is seen as an array of readonly characters. For example:
+```
+string s1 = "abc";
+readonly System.Char[] s = new System.Char[] { 'a', 'b', 'c' };
+```
+...are identical.
+
+- Strings are immutable (readonly).
+- Check with string equality with (==) or the preferred way is string.Equals(s1, s2);
