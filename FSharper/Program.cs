a93cd5a62c78dde8e9742acd291252759453b7a3
hoodielive <hoodielive@gmail.com>
2021-03-09 02:03:37 -0500

    Uhhh...

---
+++ FSharper/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Console;
+
+namespace FSharper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void TimesTable(byte number)
+        {
+            WriteLine($"This is the {number} times table: ");
+
+            for (int row = 1; row <= 12; row++)
+            {
+                WriteLine($"{row} x {number} = { row * number }");
+            }
+            WriteLine();
+        }
+
+        static void RunTimesTable()
+        {
+            bool isNumber;
+            do 
+            {
+                Write("Enter a number between 0 and 255: ");
+            }
+        }
+}
