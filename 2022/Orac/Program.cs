8dee8ad5f3fd55988daafe1aef5fe9b20f980707
hoodielive <hoodielive@gmail.com>
2022-01-26 02:13:32 -0500

    Relax.

--- 2022/Orac/Program.cs
+++ 2022/Orac/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class Orac
+{
+    public int Age = 10;
+    static readonly string TempFolder = System.IO.Path.GetTempPath();
+    static readonly int legs = 8, eyes = 2;
+}
+
+public class Test
+{
+    public const string Message = "Hello World";
+}
+
+public static double Circumference(double radius)
+{
+    return 2 * System.Math.PI * radius;
+}
+
+public static double Circumference(double radius)
+{
+    return 6.283214859530 * radius;
+}
