1df3ac97ab002736318f4c2b9f1f6c0b0d2f037d
hoodielive <hoodielive@gmail.com>
2022-05-25 00:42:28 -0400

    ...

---
+++ 2022/Newness/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+int x = 12 * 30;
+System.Console.WriteLine(x);
+
+SayHello();
+
+Console.WriteLine(FeetToInches(30));
+
+
+int FeetToInches(int feet)
+{
+   int inches = feet * 12;
+   return inches;
+}
+
+void SayHello()
+{
+    Console.WriteLine("Hello, world!");
+}
+
+string message = "Hello, World";
+string upperMessage = message.ToUpper();
+Console.WriteLine(upperMessage);
+
+int x = 2022;
+message = message + x.ToString();
+
+var node = new JsonArray
+{
+    new JsonObject {
+        ["Name"] = "Osa",
+        ["Age"] =  40,
+        ["Friends"] = new JsonArray("Oyeku", "Ogbe");
+    },
+
+    new JsonObject {
+        ["Name"] = "Otura",
+        ["Age"] = 50,
+        ["Friends"] = new JsonArray("Otura", "Irete");
+    }
+};
+
+Console.WriteLine(node);
