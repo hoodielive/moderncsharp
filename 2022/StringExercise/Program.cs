5e88ac6f45791976dfaac7e8b1244ca778b52a23
hoodielive <hoodielive@gmail.com>
2022-06-12 18:43:48 -0400

    dope

--- 2022/StringExercise/Program.cs
+++ 2022/StringExercise/Program.cs
@@ -18,3 +18,23 @@ Console.WriteLine($"[{trimmedGreetingE}]");
 Console.WriteLine($"[{greetingM}]");
 string trimmedGreetingM = greetingM.Trim();
 Console.WriteLine($"[{greetingM}]");
+
+string sayHowdy = "Hello World!";
+Console.WriteLine(sayHowdy);
+sayHowdy = sayHowdy.Replace("Hello", "Greetings");
+Console.WriteLine(sayHowdy);
+
+Console.WriteLine(sayHowdy.Contains("World!"));
+Console.WriteLine(sayHowdy.StartsWith("Greetings"));
+Console.WriteLine(sayHowdy.EndsWith("World"));
+Console.WriteLine(sayHowdy.EndsWith("Worlds"));
+Console.WriteLine(sayHowdy.EndsWith("World!"));
+int a = 5;
+int b = 4;
+int c = 2;
+int d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+Console.WriteLine(d);
+
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range rof integers id {min} to {max}");
