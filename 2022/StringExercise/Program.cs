a2d35e7320b726c1ee80ebc27007d9a7c1d6cc92
hoodielive <hoodielive@gmail.com>
2022-06-12 16:50:36 -0400

    String methods

---
+++ 2022/StringExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿string firstFriend = "Vincent";
+string secondFriend = "Jordan";
+Console.WriteLine($"Hello {firstFriend} and {secondFriend} ");
+Console.WriteLine($"The length of {firstFriend} is {firstFriend.Length}");
+
+string greeting = "                                 Howdy!";
+string greetingE = "Howdy!                                ";
+string greetingM = "            Howdy!                    ";
+Console.WriteLine($"[{greeting}]");
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+Console.WriteLine($"[{greetingE}]");
+string trimmedGreetingE = greetingE.TrimEnd();
+Console.WriteLine($"[{trimmedGreetingE}]");
+
+Console.WriteLine($"[{greetingM}]");
+string trimmedGreetingM = greetingM.Trim();
+Console.WriteLine($"[{greetingM}]");
