ce2f263af39f0d0c038dd2c2a0b150b49cd9e1bf
hoodielive <hoodielive@gmail.com>
2022-06-26 22:22:30 -0400

    Stuff.

---
+++ 2022/ArraysAnd/Person.cs
@@ -0,0 +1,11 @@
+public class Person
+{
+  public string FirstName;
+  public string LastName;
+
+
+  public void Introduce()
+  {
+    Console.WriteLine(($"My name is {FirstName} {LastName}"));
+  }
+}
---
+++ 2022/ArraysAnd/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    Person osa = new Person();
+    osa.FirstName = "Osa";
+    osa.LastName = "Oso";
+    osa.Introduce();
+  }
+}
